7ca225774748a6862f525a596357c6e25fcec1e9
Aditya4991 <adityamontysingh@gmail.com>
2020-09-10 10:09:46 +0530

    initial commit

---
+++ msgconsumer/src/main/java/com/proj/arch/msgconsumer/MsgconsumerApplication.java
@@ -0,0 +1,13 @@
+package com.proj.arch.msgconsumer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsgconsumerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsgconsumerApplication.class, args);
+	}
+
+}
---
+++ msgconsumer/src/test/java/com/proj/arch/msgconsumer/MsgconsumerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.proj.arch.msgconsumer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsgconsumerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
